784c95aebcf63aa9bfc88879ecdc4ebd516d6707
andres.march <andres.march@94ec22c2-0c42-0410-a372-15c901ad7915>
2007-12-25 02:21:08 +0000

    better reflection and some cleanup

--- bdb-browser/src/java/org/mindhaus/bdb/browser/ui/views/IndexViewContentProvider.java
+++ bdb-browser/src/java/org/mindhaus/bdb/browser/ui/views/IndexViewContentProvider.java
@@ -13,6 +13,7 @@ import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
 import org.mindhaus.bdb.browser.ui.model.IndexNode;
+import org.mindhaus.bdb.browser.ui.model.ObjectNode;
 
 import com.sleepycat.persist.EntityCursor;
 
@@ -42,25 +43,42 @@ public class IndexViewContentProvider implements IStructuredContentProvider,
 		if (parent instanceof IndexNode) {			
 			Iterable cursor = ((IndexNode)parent).getEntries();
 			for (Object entity : cursor) {
+				entity = new ObjectNode("Entity", entity.getClass(), entity);
 				children.add(entity);
 			}
+		} else if (parent instanceof ObjectNode) {
+			ObjectNode node = (ObjectNode) parent;
+			Object value = node.getValue();			
+			children = _getChildren(value);
 		} else {
-			
-			Field[] fields = parent.getClass().getDeclaredFields();
-    		if (fields != null ) {
-				for (int i = 0; i < fields.length; i++) {
-					Field field = fields[i];
-					try {
-						field.setAccessible(true);
-						Object child = field.get(parent);
-						children.add(child);
-					} catch (Throwable e) {
-						log.log(Level.SEVERE, "Error accessing field: " + parent + " - " + field);
-					}
-				}
-    		}
+			children = _getChildren(parent);
 		}
 		return children.toArray();
+	}
+
+	protected List<Object> _getChildren(Object parent) {
+		List<Object> children = new ArrayList<Object>();
+		Field[] fields = parent.getClass().getDeclaredFields();
+		if (fields != null ) {
+			for (int i = 0; i < fields.length; i++) {
+				Field field = fields[i];
+				try {
+					Object child = getChild(parent, field);						
+					children.add(child);
+				} catch (Throwable e) {
+					log.log(Level.SEVERE, "Error accessing field: " + parent + " - " + field);
+				}
+			}
+		}
+		return children;
+	}
+
+	protected Object getChild(Object parent, Field field)
+			throws IllegalAccessException {
+		field.setAccessible(true);
+		Object child = field.get(parent);
+		child = new ObjectNode(field.getName(), field.getType(), child);
+		return child;
 	}
 
     public boolean hasChildren(Object parent) {
@@ -74,4 +92,6 @@ public class IndexViewContentProvider implements IStructuredContentProvider,
     	}
     	return false;
 	}
+    
+    
 }
